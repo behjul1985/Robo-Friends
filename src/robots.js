5126a6a7f6670322b760c039abaf2ba7b3378db1
behjul1985 <65499309+behjul1985@users.noreply.github.com>
2020-07-17 09:43:59 +0100

    add project files

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots = [
+
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'LeaGraham',
+        email: 'leanne@gmail.com',
+    },
+    {
+        id: 2,
+        name: 'Lucy',
+        username: 'lucy',
+        email: 'lucy@gmail.com',
+    },
+    {
+        id:3,
+        name: 'Chris Perk',
+        username: 'ChrisPerks',
+        email: 'leanne@gmail.com',
+    },
+    {
+        id:4,
+        name: 'Jane Smith',
+        username: 'Janeysmith',
+        email: 'leanne@gmail.com',
+    },
+    {
+        id:5,
+        name: 'Michael Palin',
+        username: 'MikeyPalin',
+        email: 'leanne@gmail.com',
+    },
+    {
+        id:6,
+        name: 'Sam Straw',
+        username: 'SammyStraw',
+        email: 'samstraw@microsoft.com',
+    },   
+    {
+        id:7,
+        name: 'Tommy Jones',
+        username: 'TommyJ',
+        email: 'tommyj@hotmail.com',
+    },
+    {
+        id:8,
+        name: 'Paul Fogle',
+        username: 'PaulFogle',
+        email: 'paul.f@homail.com',
+    },
+    {
+        id:9,
+        name: 'Jenny Howard',
+        username: 'JennyHoward',
+        email: 'JennyHoward@yahoo.com',
+    }
+
+
+
+]
